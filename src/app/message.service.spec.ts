c0bdff6c51dd092c6a3fabe572fad497e6c80172
AmauryD <deflorenne.amaury@gmail.com>
2024-03-01 13:58:08 +0100

    hooks

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,12 +1,38 @@
+import { TestBed } from "@angular/core/testing";
 import { HeroService } from "./hero.service";
 import { MessageService } from "./message.service";
 
 describe('MessageService', () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+    service = new MessageService();
+  });
+
   it('adds a message when MessageService.add is called', () => {
-    const service = new MessageService();
-
     service.add('Coucou');
-
     expect(service.messages).toEqual(['Coucou']);
+  });
+
+  it('clear() empties the array', () => {
+    service.add('message');
+    service.clear();
+    expect(service.messages).toEqual([]);
+  });
+
+  afterEach(() => {
+    // cleanup
+  });
+});
+
+describe('MessageService with TestBed', () => {
+  it('adds a message when MessageService.add is called', ()  => {
+    TestBed.configureTestingModule({});
+
+    const service = TestBed.inject(MessageService);
+
+    service.add('coucou');
+
+    expect(service.messages).toEqual(['coucou']);
   });
 });
