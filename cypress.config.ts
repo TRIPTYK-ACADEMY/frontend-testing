9e2546251e9d285dc8a43c09f6daeee24bb551e8
AmauryD <deflorenne.amaury@gmail.com>
2024-03-04 10:23:26 +0100

    maison de lumière

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+import { lighthouse, prepareAudit } from "@cypress-audit/lighthouse";
 
 export default defineConfig({
 
   e2e: {
     'baseUrl': 'http://localhost:4200',
-    experimentalStudio: true
+    experimentalStudio: true,
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions);
+      });
+
+      on("task", {
+        lighthouse: lighthouse()
+      });
+    },
   },
-
-
 })
