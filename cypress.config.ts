2af273f6c36e610627aa25a96257b0c6d8d3808f
AmauryD <deflorenne.amaury@gmail.com>
2024-03-04 11:01:57 +0100

    lightouse

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   e2e: {
     'baseUrl': 'http://localhost:4200',
     experimentalStudio: true,
+    taskTimeout: 120 * 1000,
     setupNodeEvents(on, config) {
       on("before:browser:launch", (browser = {}, launchOptions) => {
         prepareAudit(launchOptions);
--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -5,5 +5,12 @@ describe('template spec', () => {
     cy.get('#search-box').type('bombasto');
     cy.get('li > a').click();
     cy.get('app-hero-detail > :nth-child(1) > h2').contains('BOMBASTO');
-  })
+  });
+
+  it('it passes lightouse tests on homepage', () => {
+    cy.visit('/');
+    cy.lighthouse({
+      performance: 100,
+    });
+  });
 })
