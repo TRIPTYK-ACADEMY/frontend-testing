ede4e7a1df948c389ec0b3f2c64e765d7848768b
AmauryD <deflorenne.amaury@gmail.com>
2024-03-04 10:02:51 +0100

    grobatar

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,21 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('Tour of Heroes')
-  })
+    cy.contains('Tour of Heroes');
+  });
+
+  it('When i click on heroes button, it should show all heroes', () => {
+    cy.visit('/');
+    cy.get('[routerlink="/heroes"]').click();
+    cy.get('.heroes > :nth-child(1)').should('exist');
+  });
+
+  it('Should add a hero to the list when clicking on "add an hero" button', () => {
+    cy.visit('/');
+    cy.get('[routerlink="/heroes"]').click();
+    cy.get('#new-hero').clear();
+    cy.get('#new-hero').type('Grobatar');
+    cy.get('.add-button').click();
+    cy.get('.heroes').contains('Grobatar')
+  });
 })
---
+++ cypress/e2e/test.cy.ts
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('#search-box').clear();
+    cy.get('#search-box').type('bombasto');
+    cy.get('li > a').click();
+    cy.get('app-hero-detail > :nth-child(1) > h2').contains('BOMBASTO');
+  })
+})
